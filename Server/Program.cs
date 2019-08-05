eb4e6db6391e50288104207a191d1239db649423
cristinacoxinho <mcncoxinho@gmail.com>
2019-08-05 19:03:58 +0100

    estudo startup

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,16 +1,13 @@
-﻿using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore; // Um namespace é um conjunto de classes :)
 using Microsoft.AspNetCore.Hosting;
 
 namespace Server {
-    public class Program {
-        public static void Main(string[] args) {
-            BuildWebHost(args).Run();
-        }
+	public class Program {
+		public static void Main(string[] args) {
+			Build(args).Run();
+		}
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args) // Static Method that creates the web host
-                .UseStartup<Startup>()
-                .UseUrls("https://localhost:5001") // We can access files at /wwwroot/
-                .Build();
-    }
+		public static IWebHost Build(string[] args)
+			=> WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().UseUrls("https://localhost:5001").Build();
+	}
 }
